27b322a1809d2b2e4d4d5669eb416a62dee0f2ab
TJBrodrick <timothy.brodrick@gmail.com>
2020-05-19 09:32:56 -0500

    Working in the forEach area.

---
+++ js/ratings.js
@@ -0,0 +1,12 @@
+const collection_ratings = () => {
+const ratings{'count' = 0,'sum' = 0,'average' = 0};
+ let rating = 0;
+ const elements.document.querySelectAll('.rating');
+  elements.foreach(element-> {
+  rating = parseInt(elements.id.replace('star',''));
+  });
+ )
+
+
+ replace()
+}
